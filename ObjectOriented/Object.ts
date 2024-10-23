bc9984ecbc20581ab7bac60a6b0ca4807a395a68
YasminZahrir <yas.zahrir@gmail.com>
2024-10-23 15:17:47 +0200

    huh

--- ObjectOriented/Object.ts
+++ ObjectOriented/Object.ts
@@ -4,6 +4,13 @@ namespace VectorObject {
         x: number;
         y: number;
     
+        constructor(_x: number, _y: number) {
+            this.set(_x, _y);
+        }
+        set(_x: number, _y: number): void {
+            this.x = _x;
+            this.y = _y;
+        }
         scale(_factor: number): void {
             this.x *= _factor;
             this.y *= _factor;
@@ -14,7 +21,7 @@ namespace VectorObject {
             this.y += _addend.y;
         }
     }
-    const v1: Vector = new Vector();
+    const v1: Vector = new Vector(7,3);
     v1.scale(2);
     console.log(v1);
 }
