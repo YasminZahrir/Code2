c2e92b8bb93321c05f815f4ecbf0ae836d390bc9
YasminZahrir <yas.zahrir@gmail.com>
2024-12-16 17:32:44 +0100

    sda

--- Dice/Dice.js
+++ Dice/Dice.js
@@ -1,8 +1,8 @@
 export var Yasmin;
 (function (Yasmin) {
     function strategy(_score, _active, _potential) {
-        if (_potential > 50) {
-            return Math.random() < 1;
+        if (_potential > 15) {
+            return Math.random() < 0.8;
         }
         else {
             return Math.random() < 0.5;
--- Dice/Dice.ts
+++ Dice/Dice.ts
@@ -1,7 +1,7 @@
 export namespace Yasmin {
     export function strategy (_score:number[], _active:number, _potential:number): boolean {
-        if (_potential > 50) {
-            return Math.random() < 1
+        if (_potential > 15) {
+            return Math.random() < 0.8
         } else {
             return Math.random() < 0.5
         }
